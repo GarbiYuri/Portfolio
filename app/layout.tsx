e7ea08bb2d5262bd46ef7de7d36729cba4f2ad90
Francisco Yuri Garbi <franciscoyuri145@gmail.com>
2026-03-16 09:31:37 -0300

    feat: Add Icon and Title Site

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <title>Meu Portifolio</title>
+      <link rel="shortcut icon" href="/eu.jpeg" type="image/x-icon" />
       <body>
         {children}
       </body>
